6ce450d1d58c86d31e0d124fa27d3b4a6f777b57
FaresMer <merfares04@gmail.com>
2025-07-31 19:07:15 +0100

    calculator without comma and dlt

---
+++ src/Calculator.jsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+
+function Calculator(){
+    const [a,setA] = useState(0);
+    const [oper,setOper] = useState('');
+    const [isEqual,setIsEqual] = useState(false);
+
+
+    function addNumber(e){
+        isEqual?document.getElementsByClassName('result')[0].value='':null;
+        const n = e.target.innerText;
+        document.getElementsByClassName('result')[0].value+=n;
+        setIsEqual(false);
+
+    }
+
+    function clear(){
+        document.getElementsByClassName('result')[0].value = '';
+        setA(0);
+    }
+
+    function operation(e){
+        setA(parseInt(document.getElementsByClassName('result')[0].value));
+        setOper(e.target.innerText);
+        document.getElementsByClassName('result')[0].value = '';
+
+        
+
+
+    }
+
+    function equals(){
+           const b=parseInt(document.getElementsByClassName('result')[0].value);
+           switch (oper) {
+            case '+':
+                document.getElementsByClassName('result')[0].value = a+b;
+                
+                break;
+            case '-':
+                document.getElementsByClassName('result')[0].value = a-b;
+                
+                break;
+            case '*':
+                document.getElementsByClassName('result')[0].value = a*b;
+                
+                break;
+            case '/':
+                document.getElementsByClassName('result')[0].value = a/b;
+                
+                break;
+            default:
+                break;
+           }
+           setIsEqual(true);
+
+    }
+
+
+
+
+
+    return(<>
+        <div className="calculator">
+            <input type="text" className="result" readOnly placeholder="00000000"/>
+            <div>
+                <button className="" onClick={clear}>AC</button>
+                <button className="">%</button>
+                <button className="">DLT</button>
+                <button className=""  onClick={operation}>/</button>
+            </div>
+            <div>
+                <button className="number" onClick={addNumber}>7</button>
+                <button className="number" onClick={addNumber}>8</button>
+                <button className="number" onClick={addNumber}>9</button>
+                <button className="" onClick={operation}>*</button>
+            </div>
+            <div>
+                <button className="number" onClick={addNumber}>4</button>
+                <button className="number" onClick={addNumber}>5</button>
+                <button className="number" onClick={addNumber}>6</button>
+                <button className="" onClick={operation}>-</button>
+            </div>
+            <div>
+                <button className="number" onClick={addNumber}>1</button>
+                <button className="number" onClick={addNumber}>2</button>
+                <button className="number" onClick={addNumber}>3</button>
+                <button className="" onClick={operation}>+</button>
+            </div>
+            <div>
+                <button className="number" onClick={addNumber}>00</button>
+                <button className="number" onClick={addNumber}>0</button>
+                <button className="">.</button>
+                <button className="" onClick={equals}>=</button>
+            </div>
+            
+        </div>
+    </>)
+}  
+
+export default Calculator;
